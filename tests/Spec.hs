e132a5ba693ba43e23541132f1fe6f74eb32250d
wtanaka.com <wtanaka@yahoo.com>
2016-11-18 21:04:25 -1000

    Fix warnings

--- tests/Spec.hs
+++ tests/Spec.hs
@@ -34,7 +34,7 @@ qcProps = testGroup "(checked by QuickCheck)"
   [ QC.testProperty "sort == sort . reverse" $
       \list -> sort (list :: [Int]) == sort (reverse list)
   , QC.testProperty "Fermat's little theorem" $
-      \x -> ((x :: Integer)^7 - x) `mod` 7 == 0
+      \x -> ((x :: Integer)^(7 :: Integer) - x) `mod` 7 == 0
   -- the following property does not hold
   -- , QC.testProperty "Fermat's last theorem" $
   --     \x y z n ->
@@ -44,7 +44,7 @@ qcProps = testGroup "(checked by QuickCheck)"
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
   [ testCase "List comparison (different length)" $
-      [1, 2, 3] `compare` [1,2] @?= GT
+      ([1, 2, 3] :: [Integer]) `compare` ([1,2] :: [Integer]) @?= GT
 
   -- the following test does not hold
   --, testCase "List comparison (same length)" $
